79b88e3fbade78f6fb2b9dc5d506027d885bb19e
zzl <1120277189@qq.com>
2016-11-16 14:39:47 +0800

    第一次提交

---
+++ app/src/main/java/edu/feicui/interview/MainActivity.java
@@ -0,0 +1,19 @@
+package edu.feicui.interview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
+/**
+ * 1.屏幕大小：屏幕对角线长度  单位
+ * 2.分辨率：屏幕的宽*屏幕的高   手机屏幕大小相同 分辨率越高越清晰  手机分辨率相同  手机屏幕越小越明白
+ * 3.计算公式
+ */
